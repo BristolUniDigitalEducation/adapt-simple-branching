15dd613409c54679707ee5bb96d6d94ba35c214c
SteveX <stevex@ultrajam.net>
2018-07-18 14:07:05 +0100

    Update adapt-simple-branchingBlockModel.js

--- js/adapt-simple-branchingBlockModel.js
+++ js/adapt-simple-branchingBlockModel.js
@@ -1,140 +1,221 @@
 define([
     'coreJS/adapt',
-    'coreViews/blockView'
-], function (Adapt, CoreBlockView) {
-    var SimpleBranchingView = {
-        postRender: function () {
-            //console.log("SimpleBranchingView"+":"+this.model.get("_id"), "postRender");
-            CoreBlockView.prototype.postRender.call(this);
-            if (this.model.isBranchingEnabled()) {
-                this.hideComponents();
-                if (this.model.isQuestionComplete()) {
-                    //display appriopriate component
-                    this.showComponent();
-                } else {
-                    //listen to finish of question
-                    this.listenTo(Adapt, "remove", this.onRemove);
-                    this.listenTo(this.model.getQuestionModel(), "change:_isInteractionComplete", this.onQuestionComplete);
-                }
+    'coreModels/blockModel'
+], function (Adapt, CoreBlockModel) {
+    var BRANCHING_ID = "_branching";
+    var SimpleBranchingModel = {
+
+
+        setupModel: function () {
+            CoreBlockModel.prototype.setupModel.call(this);
+            var o = this.get(BRANCHING_ID);
+            if (!o.hasOwnProperty("_isEnabled")) {
+                o._isEnabled = true;
+            }
+            if (!o.hasOwnProperty("forceCompletion")) {
+                o.forceCompletion = true;
             }
         },
-        hideComponents: function () {
-           //console.log("SimpleBranchingView" + ":" + this.model.get("_id"), "hideComponents");
+        setCompletionStatus: function () {
+            this.set({
+                "_isComplete": true,
+                "_isInteractionComplete": true,
+            });
+        },
+
+        //PUBLIC
+        isBranchingEnabled: function () {
+            var o = this.get(BRANCHING_ID);
+            if (o && o._isEnabled && this.isConfigValid()) return true;
+            return false;
+        },
+        getConfig: function () {
+            var config = this.get(BRANCHING_ID);
+
+            return config;
+        },
+        /**
+         * Checks if the config object passed from JSON is valid
+         */
+        isConfigValid: function () {
+            var config = this.getConfig(),
+                id = this.get("_id"),
+                result = true;
             //
-            var descendantComponents = this.model.findDescendants('components');
-            if (descendantComponents.length > 0) {
-                descendantComponents.each(_.bind(function (model) {
-                    model.set("_isAvailable", false);
-                    model.set("_isVisible", false);
-                    try {
-                        this.$("." + model.get("_id")).addClass("display-none");
-                    } catch (e) {
-                        console.error(e);
+            if (!config.hasOwnProperty("questionId")) {
+                console.error("BranchingBlockModel", "Missing 'questionId' property in block '" + id + "'.");
+                return false;
+            } else {
+                //check if it exists and is NOT child of this block
+                var questionModel;
+                try {
+                    questionModel = Adapt.findById(config.questionId);
+                } catch (e) {}
+
+                if (!questionModel) {
+                    console.error("BranchingBlockModel", "There is no component mentioned in 'questionId' ('" + config.questionId + "') for block '" + id + "'.");
+                    return false;
+                }
+
+                if (questionModel.get("_parentId") === this.get("_id")) {
+                    console.error("BranchingBlockModel", "Component mentioned in 'questionId' may not be a child of block '" + id + "'.");
+                    //return false;
+                }
+            }
+            //
+            if (!config.hasOwnProperty("correct")) {
+                console.error("BranchingBlockModel", "Missing 'correct' property in block '" + id + "'.");
+                return false;
+            } else {
+                //check if it exists and is child of this block
+                if (this.isUsingCorrect()) {
+                    result = this._checkCorrectModel(config.correct, id);
+                }
+            }
+            //
+            if (!config.hasOwnProperty("incorrect")) {
+                console.error("BranchingBlockModel", "Missing 'incorrect' property in block '" + id + "'.");
+                return false;
+            } else {
+                //check if it exists and is child of this block
+                if (this.isUsingIncorrect()) {
+
+                    result = this._checkIncorrectModel(config.incorrect, id);
+
+                }
+            }
+            //
+            return result;
+        },
+
+
+        isQuestionComplete: function () {
+            var questionModel = this.getQuestionModel();
+            return questionModel ? questionModel.get("_isComplete") && questionModel.get("_isInteractionComplete") : false;
+        },
+        isQuestionCorrect: function () {
+            var questionModel = this.getQuestionModel();
+
+            return questionModel ? questionModel.get("_isCorrect") : false;
+        },
+        /**
+         * Returns Question model
+         */
+        getQuestionModel: function () {
+
+            var config = this.getConfig();
+
+            return this._getModel(config.questionId);
+        },
+
+        isUsingCorrect: function () {
+            var config = this.getConfig();
+            return config.correct !== "";
+        },
+        isUsingIncorrect: function () {
+            var config = this.getConfig();
+            return config.incorrect !== "";
+        },
+        isForceCompletion: function () {
+            var config = this.getConfig();
+            return config.forceCompletion;
+        },
+
+
+
+        /**
+         * An array of models associated with correct answer
+         */
+        getCorrectModel: function () {
+            if (!this.isUsingCorrect()) return;
+            var config = this.getConfig(),
+            ids = config.correct;
+
+            return this._getModels(ids);
+        },
+        /**
+         * An array of models associated with incorrect answer
+         */
+        getIncorrectModel: function () {
+            if (!this.isUsingIncorrect()) return;
+
+            var config = this.getConfig(),
+                ids = config.incorrect;
+
+            return this._getModels(ids);
+        },
+
+        //PRIVATE
+        _checkCorrectModel: function (ids, id) {
+            return this._checkModel(ids, id, "correct");
+        },
+        _checkIncorrectModel: function (ids, id) {
+            return this._checkModel(ids, id, "incorrect");
+        },
+        _checkModel: function (ids, id, type) {
+            //console.info("BranchingBlockModel", "_checkModel", arguments);
+            var model;
+            if (ids.indexOf(",") == -1) {
+
+                model = this._getModel(ids);
+
+                if (!model) {
+                    console.error("BranchingBlockModel", "There is no component mentioned in '" + type + "' ('" + ids + "') for block '" + id + "'.");
+                    return false;
+                }
+
+                if (model.get("_parentId") !== this.get("_id")) {
+                    console.error("BranchingBlockModel", "Component mentioned in '" + type + "' may not be a child of block '" + id + "'.");
+                    //return false;
+                }
+            } else {
+                var listIds = ids.split(",");
+                var result = false,
+                    i = 0;
+
+                while (listIds.length > 0) {
+                    var li = listIds.pop();
+                    result = this._checkModel(li, id, type);
+                    if (!result) break;
+                }
+
+                return result;
+            }
+            return true;
+        },
+
+        _getModels: function (ids) {
+
+            if (ids.indexOf(",") == -1) {
+                var model = this._getModel(ids);
+                if (model) return [model];
+            } else {
+
+                var listIds = ids.split(","),
+                    i = 0,
+                    result = [];
+
+                while (listIds.length > 0) {
+                    var id = listIds.pop();
+                    var model = this._getModel(id);
+
+                    if (!model) {
+                        return;
+                    } else {
+                        result.push(model);
                     }
-                }), this);
-            }
-            //
-            this.model.set("_isVisible", false);
-            this.$el.addClass("display-none");
-            //
-            Adapt.trigger("pageLevelProgress:update");
-        },
-        showComponent: function () {
-           //console.log("SimpleBranchingView" + ":" + this.model.get("_id"), "showComponent");
-           //console.log("SimpleBranchingView" + ":" + this.model.get("_id"), this.model);
-            var models,
-                correct = this.model.getCorrectModel(),
-                incorrect = this.model.getIncorrectModel(),
-                forceCompletion = this.model.isForceCompletion();
-           //console.log("SimpleBranchingView" + ":" + this.model.get("_id"), "forceCompletion", forceCompletion);
-            if (forceCompletion) {
-                if (correct) this._setCompletionOnModel(correct, true);
-                if (incorrect) this._setCompletionOnModel(incorrect, true);
-            } else {
-                //force completion of "hidden" child
-                if (!this.model.isQuestionCorrect()) {
-                    if (correct) this._setCompletionOnModel(correct, true);
-                } else {
-                    if (incorrect) this._setCompletionOnModel(incorrect, true);
                 }
+                return result;
             }
+            return;
+        },
+        _getModel: function (id) {
+            try {
+                var model = Adapt.findById(id);
+            } catch (e) { console.error("BranchingBlockModel:" + this.get("_id"), e); }
 
-            if (this.model.isQuestionCorrect()) {
-                //show 'correct' component
-                models = correct;
-            } else {
-                //show 'incorrect' component
-                models = incorrect;
-            }
-            //disable hidden children
-            if (!this.model.isQuestionCorrect()) {
-                if (correct) this._disableModel(correct);
-            } else {
-                if (incorrect) this._disableModel(incorrect);
-            }
-            //
-           //console.log("SimpleBranchingView" + ":" + this.model.get("_id"), "models", models);
-            //
-            if (models && models.length > 0) {
-
-                for (var i = 0, max = models.length; i < max; i++) {
-                    var m = models[i];
-                    m.set("_isVisible", true);
-                    this.$("." + m.get("_id")).removeClass("display-none");
-                    m.set("_isAvailable", true);
-                }
-				// moved this inside condition or block titles will
-				// show for blocks with no components
-				this.model.set("_isVisible", true);
-				this.$el.removeClass("display-none");
-            }
-            //
-            if (forceCompletion) {
-                this.model.setCompletionStatus();
-            }
-            //
-            Adapt.trigger("pageLevelProgress:update");
-            //
-            $(window).resize();
-        },
-        _setCompletionOnModel: function (models, status) {
-           //console.log("SimpleBranchingView" + ":" + this.model.get("_id"), "_setCompletionOnModel(models, status)", models, status);
-            if (models && models.length > 0) {
-                for (var i = 0, max = models.length; i < max; i++) {
-                    var model = models[i];
-                    model.set({
-                        "_isVisible": status,
-                        "_isComplete": status,
-                        "_isInteractionComplete": status
-                    });
-                }
-            }
-        },
-        _disableModel: function (models) {
-           //console.log("SimpleBranchingView" + ":" + this.model.get("_id"), "_disableModel(models)", models);
-            if (models && models.length > 0) {
-                for (var i = 0, max = models.length; i < max; i++) {
-                    var model = models[i];
-                    model.set({
-                        "_isEnabled": false,
-                        "_navButtons": {
-                            "_isEnabled": false
-                        }
-                    });
-                }
-            }
-        },
-        onQuestionComplete: function (model, value, isPerformingCompletionQueue) {
-           //console.log("SimpleBranchingView" + ":" + this.model.get("_id"), "onQuestionComplete", arguments);
-            if (value) {
-                this.showComponent();
-            }
-        },
-        onRemove: function () {
-            this.stopListening(this.model.getQuestionModel(), "change:_isInteractionComplete", this.onQuestionComplete);
-            this.stopListening(Adapt, "remove", this.onRemove);
+            return model;
         }
-    };
-
-    return SimpleBranchingView;
+    }
+    return SimpleBranchingModel;
 });
